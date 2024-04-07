7f40d6ca32fbeb4bf515b0e8af7b2be206e2a9d4
joluhan <135083872+joluhan@users.noreply.github.com>
2024-04-07 15:49:55 +0200

    Add custom User model with role field
    - Imported `AbstractUser` from `django.contrib.auth.models`
    - Created custom `User` model extending `AbstractUser`
      - Added `fullname` field as a CharField with max length 255
      - Added `username` field as a CharField with max length 150
      - Added `role` field with choices for 'sales', 'support', 'management'
      - Set `USERNAME_FIELD` to 'username'
      - Overrode `__str__` method to return `username`

--- EpicEvents/models.py
+++ EpicEvents/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+# Importing necessary classes from django.db and django.contrib.auth.models
+from django.db import models # Importing models from django.db
+from django.contrib.auth.models import AbstractUser # Importing AbstractUser from django.contrib.auth.models
 
-# Create your models here.
+# Definition of custom User model, extending AbstractUser to include additional fields
+class User(AbstractUser):
+    # Role choices defined for the User model
+    ROLE_CHOICES = [
+        ('sales', 'Sales'),
+        ('support', 'Support'),
+        ('management', 'Management'),
+    ]
+
+    # Specifies the field used as the unique identifier for login
+    USERNAME_FIELD = 'username'
+    # Additional fields for the User model
+    fullname = models.CharField(max_length=255, unique=True) # Full name of the user
+    username = models.CharField(max_length=150, unique=True) # Username of the user
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES) # Role of the user
+
+    # Human-readable representation of the User object
+    def __str__(self):
+        return self.username # Return the username of the user
